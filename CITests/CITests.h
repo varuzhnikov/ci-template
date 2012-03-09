47ee0b40fea5575c858540980224eccad1e40fe8
Vitaly Ruzhnikov <varuzhnikov@gmail.com>
2012-03-09 21:09:15 +0300

    Missing files

---
+++ CITests/CITests.h
@@ -0,0 +1,13 @@
+//
+//  CITests.h
+//  CITests
+//
+//  Created by Виталий on 05.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CITests : SenTestCase
+
+@end
---
+++ CITests/CITests.m
@@ -0,0 +1,32 @@
+//
+//  CITests.m
+//  CITests
+//
+//  Created by Виталий on 05.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CITests.h"
+
+@implementation CITests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STAssertTrue(YES, @"Unit tests are not implemented yet in CITests");
+}
+
+@end
